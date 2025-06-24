10dd48492c27b1436b3e4efd8125d5503c0d55c3
Mohit Yadav <mohityadav@Mohits-MacBook-Air.local>
2025-06-25 00:00:05 +0530

    fetch user on profile page

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,8 +1,35 @@
 import NavBar from './NavBar'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useNavigate } from 'react-router-dom'
 import Footer from './Footer'
+import { useEffect } from 'react';
+import { BASE_URL } from '../utils/constants';
+import { useDispatch, useSelector } from 'react-redux';
+import { addUser } from '../utils/userSlice';
+import axios from 'axios';
 
 const Body = () => {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const userData = useSelector((store) => store.user);
+    const fetchUser = async () => {
+        if(userData) return;
+        try {
+            const res = await axios.get(BASE_URL + '/profile/view', {
+                withCredentials: true
+            });
+            dispatch(addUser(res.data));
+            console.log("User fetched successfully:", res.data);
+        } catch (error) {
+            if(error.status === 401){
+                navigate('/login');
+            }
+            console.error("Error fetching user:", error);
+        }
+    };
+
+    useEffect(() => {
+        fetchUser();
+    }, []);
     return (
         <div>
             <NavBar />
@@ -12,4 +39,5 @@ const Body = () => {
     )
 }
 
-export default Body
+export default Body;
+
